f56632187c100f2e56abf29c19ee8fa1bd56ec24
jota_sk <jota_sk@hotmail.com>
2016-05-17 16:33:29 +0100

    main class and game state manager

--- core/src/com/github/jotask/tusk/Tusk.java
+++ core/src/com/github/jotask/tusk/Tusk.java
@@ -1,42 +1,82 @@
 package com.github.jotask.tusk;
 
-import com.badlogic.gdx.Game;
+import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.utils.Timer;
+import com.github.jotask.tusk.engine.Debug;
+import com.github.jotask.tusk.engine.GameStateManager;
+import com.github.jotask.tusk.util.Constants;
 
-public class Tusk extends Game {
+public class Tusk extends ApplicationAdapter {
 
-	public enum STATE {SPLASH, MENU, OPTIONS, PLAY, EXIT}
+	public static boolean DEBUG = Constants.DEFAULT_DEBUG;
+
+	private GameStateManager gsm;
+
+	private Debug debug;
+
+	private SpriteBatch sb;
+	private ShapeRenderer sr;
 
 	@Override
-	public void create () {
-		this.changeState(STATE.SPLASH);
+	public void create() {
+		sb = new SpriteBatch();
+		sr = new ShapeRenderer();
+		sr.setAutoShapeType(true);
+
+		gsm = GameStateManager.get();
+
+		debug = new Debug();
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	public void resize(int width, int height) {
+
 	}
 
-	public void changeState(STATE state){
-		Screen screen = null;
-		switch (state){
-			case EXIT:
-				break;
-			case PLAY:
-				break;
-			case OPTIONS:
-				break;
-			case MENU:
-				break;
-			default:
-			case SPLASH:
-				screen = new Splash();
-				break;
+	@Override
+	public void render() {
+
+		if(Gdx.input.isKeyJustPressed(Input.Keys.Q)){
+			DEBUG = !DEBUG;
 		}
-		this.setScreen(screen);
+
+		gsm.update();
+		debug.update();
+
+		Color bgColor = gsm.getState().getBgColor();
+		Gdx.gl.glClearColor(bgColor.r, bgColor.g, bgColor.b, bgColor.a);
+		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		gsm.render(sb);
+		if(this.debug.isEnabled())
+			debug.render(sb);
+
+		if(DEBUG) {
+			gsm.debug(sr);
+			if(this.debug.isEnabled())
+				debug.debug(sr);
+		}
 	}
 
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
+	@Override
+	public void dispose() {
+		gsm.dispose();
+	}
 }
---
+++ core/src/com/github/jotask/tusk/engine/GameStateManager.java
@@ -0,0 +1,86 @@
+package com.github.jotask.tusk.engine;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.github.jotask.tusk.states.menu.Menu;
+import com.github.jotask.tusk.states.options.Options;
+import com.github.jotask.tusk.states.play.Play;
+import com.github.jotask.tusk.states.selectplayer.SelectPlayer;
+import com.github.jotask.tusk.states.splash.Splash;
+import com.github.jotask.tusk.util.Constants;
+
+public class GameStateManager{
+
+    private static GameStateManager instance;
+
+    private AbstractState state;
+
+    private GameStateManager() {
+        changeState(Constants.DEFAULT_STATE);
+    }
+
+    public void update(){
+        state.update();
+    }
+
+    public void render(SpriteBatch sb){
+        sb.begin();
+        sb.setProjectionMatrix(state.getCamera().combined);
+        state.render(sb);
+        sb.end();
+    }
+
+    public void debug(ShapeRenderer sr){
+        sr.begin();
+        sr.setProjectionMatrix(state.getCamera().combined);
+        state.debug(sr);
+        sr.end();
+    }
+
+    public void changeState(STATE state){
+        AbstractState screen;
+        switch (state){
+            case PLAY:
+                screen = new Play();
+                break;
+            case OPTIONS:
+                screen = new Options();
+                break;
+            case MENU:
+                screen = new Menu();
+                break;
+            case SELECTPLAYER:
+                screen = new SelectPlayer();
+                break;
+            default:
+            case SPLASH:
+                screen = new Splash();
+                break;
+        }
+
+        // FIXME
+//        if(state != null)
+//            this.state.dispose();
+
+        this.state = screen;
+
+        this.state.init();
+    }
+
+    public static GameStateManager get(){
+        if(instance == null)
+            instance = new GameStateManager();
+        return instance;
+    }
+
+    public AbstractState getState(){
+        return state;
+    }
+
+    public void dispose(){
+        if(state != null)
+            state.dispose();
+        instance = null;
+    }
+
+}
