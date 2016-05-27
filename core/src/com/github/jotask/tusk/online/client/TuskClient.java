59a66045d3e9c0e39ab7daef83c881831939df61
jota_sk <jota_sk@hotmail.com>
2016-05-27 20:00:13 +0100

    created basic classes

---
+++ core/src/com/github/jotask/tusk/online/client/TuskClient.java
@@ -0,0 +1,7 @@
+package com.github.jotask.tusk.online.client;
+
+/**
+ * Created by Jota on 27/05/2016.
+ */
+public class TuskClient {
+}
---
+++ core/src/com/github/jotask/tusk/online/server/TuskServer.java
@@ -0,0 +1,7 @@
+package com.github.jotask.tusk.online.server;
+
+/**
+ * Created by Jota on 27/05/2016.
+ */
+public class TuskServer {
+}
